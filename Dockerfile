c7a90bcc55002b3077c56633489445f58f3c8549
Your Name <you@example.com>
2021-08-05 15:14:28 +0800

    add websocket

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# Use the official maven/Java 8 image to create a build artifact.
+# https://hub.docker.com/_/maven
+FROM maven:3.5-jdk-8-alpine as builder
+
+# Copy local code to the container image.
+WORKDIR /app
+COPY we-api .
+COPY gradlew.bat .
+COPY build.gradle .
+COPY settings.gradle .
+COPY gradlew .
+
+# Build a release artifact.
+RUN gradle build -DskipTests
+
+# Run the web service on container startup.
+CMD ["java","-Djava.security.egd=file:/dev/./urandom","-Dserver.port=8080","-jar","/app/target/we-api-1.0-SNAPSHOT-boot.jar"]
+
---
+++ we-api/src/main/java/com/we/api/service/TestService.java
@@ -0,0 +1,61 @@
+package com.we.api.service;
+
+
+import com.google.gson.Gson;
+import com.we.api.websocket.server.WebScoketHelper;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.util.Map;
+
+@RestController
+public class TestService {
+
+    private final Logger log = LoggerFactory.getLogger(WebScoketHelper.class);
+    
+    @RequestMapping("/")
+    public Map sayHello(@RequestParam(name = "top") int top) throws Exception {
+        log.info("top：" + top);
+        if (top >= 10) {
+            throw new Exception("top over 10 @Sl4j");
+        }
+
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        URI uri = new URIBuilder("https://i.news.qq.com/trpc.qqnews_web.kv_srv.kv_srv_http_proxy/list")
+                .setParameter("sub_srv_id", "24hours")
+                .setParameter("srv_id", "pc").setParameter("offset", "0").setParameter("limit", String.valueOf(top)).setParameter("strategy", "1")
+                .setParameter("ext", "{\"pool\":[\"top\"],\"is_filter\":7,\"check_type\":true}")
+                .build();
+
+        HttpGet httpGet = new HttpGet(uri);
+        CloseableHttpResponse response = null;
+        String content = "";
+        try {
+            response = httpclient.execute(httpGet);
+            if (response.getStatusLine().getStatusCode() == 200) {
+                content = EntityUtils.toString(response.getEntity(), "UTF-8");
+                log.info("内容长度：" + content);
+            }
+        } finally {
+            if (response != null) {
+                response.close();
+            }
+            httpclient.close();
+        }
+
+        Gson gson = new Gson();
+        Map map = null;
+        map = gson.fromJson(content, Map.class);
+        return map;
+    }
+}
