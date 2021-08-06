e820cd9ebcb39ca678c5b703ca7de4f8df429e95
Your Name <you@example.com>
2021-08-06 13:45:38 +0800

    add wss

--- we-api/src/main/java/com/we/api/websocket/server/WebScoketHelper.java
+++ we-api/src/main/java/com/we/api/websocket/server/WebScoketHelper.java
@@ -7,37 +7,73 @@ import org.slf4j.LoggerFactory;
 import javax.websocket.EncodeException;
 import javax.websocket.Session;
 import java.io.IOException;
-import java.util.Collection;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.function.Consumer;
 
 public class WebScoketHelper {
     private final Logger logger = LoggerFactory.getLogger(WebScoketHelper.class);
+    //concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。
+    private CopyOnWriteArraySet<WebSocketServer> clientMap = new CopyOnWriteArraySet<WebSocketServer>();
+    private static WebScoketHelper instance = null;
+
+    public static WebScoketHelper getInstance() {
+        if (instance == null) {
+            synchronized (WebScoketHelper.class) {
+                if (null == instance) {
+                    instance = new WebScoketHelper();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public int getSize() {
+        return clientMap.size();
+    }
+
+    public void add(WebSocketServer server) {
+        clientMap.add(server);
+    }
+
+    public void remove(WebSocketServer server) {
+        clientMap.remove(server);
+    }
 
     /**
      * 推送文本消息给指定的客户端
      */
-    public static void sendMessage(Session sess, String message) throws IOException {
+    public void sendMessage(Session sess, String message) throws IOException {
         sess.getBasicRemote().sendText(message);
     }
 
     /**
      * 推送对象给指定的客户端
+     *
      * @param sess:
      * @param object:
      */
-    public static void sendObject(Session sess, Object object) throws IOException, EncodeException {
+    public void sendObject(Session sess, Object object) throws IOException, EncodeException {
         sess.getBasicRemote().sendObject(object);
     }
 
     /**
-     * 推送文本给所有的客户端
+     * 私发
      */
-    public static void sendTextToAll(Collection<Session> sessionList, String message) {
-
-        sessionList.forEach(e -> {
-            try {
-                sendMessage(e, message);
-            } catch (IOException e1 ) {
-                throw new RuntimeException("推送失败，session.id=" + e.getId(), e1);
+    public void sendTextToOne(WebSocketServer target,String message) {
+        clientMap.forEach(new Consumer<WebSocketServer>() {
+            @Override
+            public void accept(WebSocketServer server) {
+                if (server == null || target ==null)
+                    return;
+                if (server.getUserId() == null || target.getUserId() == null)
+                    return;
+                if (server.userId.equals(target.getUserId())) {
+                    try {
+                        sendMessage(target.getSession(),message);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         });
     }
@@ -45,13 +81,31 @@ public class WebScoketHelper {
     /**
      * 推送文本给所有的客户端
      */
-    public static void sendObjectToAll(Collection<Session> sessionList, String message) {
+    public void sendTextToAll(String message) {
+        clientMap.forEach(new Consumer<WebSocketServer>() {
+            @Override
+            public void accept(WebSocketServer e) {
+                try {
+                    sendMessage(e.getSession(), message);
+                } catch (IOException e1) {
+                    throw new RuntimeException("推送失败，session.id=" + e.getSession().getId(), e1);
+                }
+            }
+        });
+    }
 
-        sessionList.forEach(e -> {
-            try {
-                sendObject(e, message);
-            } catch (IOException | EncodeException e1) {
-                throw new RuntimeException("推送失败，session.id=" + e.getId(), e1);
+    /**
+     * 推送文本给所有的客户端
+     */
+    public void sendObjectToAll(String message) {
+        clientMap.forEach(new Consumer<WebSocketServer>() {
+            @Override
+            public void accept(WebSocketServer e) {
+                try {
+                    sendObject(e.getSession(), message);
+                } catch (IOException | EncodeException e1) {
+                    throw new RuntimeException("推送失败，session.id=" + e.getSession().getId(), e1);
+                }
             }
         });
     }
--- we-api/src/main/java/com/we/api/websocket/server/WebSocketServer.java
+++ we-api/src/main/java/com/we/api/websocket/server/WebSocketServer.java
@@ -8,28 +8,33 @@ import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
 
-@ServerEndpoint(value = "/websocket/test/{sid}",encoders = ObjectMessageEncoder.class)
+//https://blog.csdn.net/u012977315/article/details/84944708
+@ServerEndpoint(value = "/websocket/{userId}",encoders = ObjectMessageEncoder.class)
 @Component
 public class WebSocketServer {
-    private final Logger log = LoggerFactory.getLogger(WebScoketHelper.class);
-    /**
-     * 每个客户端对应的WebSocket对象
-     */
-    public static Map<String, Session> clientMap = new ConcurrentHashMap<>();
+    private static final Logger log = LoggerFactory.getLogger(WebScoketHelper.class);
+    //静态变量，用来记录当前在线连接数。应该把它设计成线程安全的。
+    public static int onlineCount = 0;
+
+    //与某个客户端的连接会话，需要通过它来给客户端发送数据
+    public Session session;
+    //接收参数中的用户ID
+    public String userId;
+
 
     /**
      * 连接建立成功调用的方法
      */
     @OnOpen
-    public void onOpen(Session session, @PathParam("sid") String sid) {
-        clientMap.put(sid, session);
-
-        log.info("有一个新的客户端 : " + sid + " , 当前客户端连接总数: " + clientMap.size());
+    public void onOpen(Session session, @PathParam("userId") String userId) {
+        this.session = session;
+        this.userId = userId;
+        WebScoketHelper.getInstance().add(this);
+        log.info("有一个新的客户端 : " + userId + " , 当前客户端连接总数: " + WebScoketHelper.getInstance().getSize());
         try {
-            WebScoketHelper.sendMessage(session, "你好，新客户端！sid = " + sid);
+            WebScoketHelper.getInstance().sendMessage(session, "你好，新客户端！userId = " + userId);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -39,9 +44,9 @@ public class WebSocketServer {
      * 连接关闭调用的方法
      */
     @OnClose
-    public void onClose(@PathParam("sid") String sid) {
-        clientMap.remove(sid);
-        log.info("有一个客户端断开，当前客户端连接总数: " + clientMap.size());
+    public void onClose(@PathParam("userId") String userId) {
+        WebScoketHelper.getInstance().remove(this);
+        log.info("有一个客户端断开，当前客户端连接总数: " + WebScoketHelper.getInstance().getSize());
     }
 
     /**
@@ -49,8 +54,13 @@ public class WebSocketServer {
      * @param message 客户端发送过来的消息
      */
     @OnMessage
-    public void onMessage(String message, @PathParam("sid") String sid) {
-        log.info("接收到客户端消息, " + sid +" , message: " + message);
+    public void onMessage(String message, @PathParam("userId") String userId) {
+        log.info("接收到客户端消息, " + userId +" , message: " + message);
+        try {
+            WebScoketHelper.getInstance().sendMessage(session,message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -60,8 +70,15 @@ public class WebSocketServer {
      */
     @OnError
     public void onError(Session session, Throwable error) {
-        log.error("出错了！");
+        log.error("出错了！"+(error!=null?error.getMessage():null));
         error.printStackTrace();
     }
 
+    public Session getSession() {
+        return session;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
 }
